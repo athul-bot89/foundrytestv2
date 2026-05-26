f5405481c04c823b4f5d941642e69b7033071668
athul-bot89 <athul-bot89@users.noreply.github.com>
2026-05-26 22:06:02 +0530

    back to normal with out token

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,12 +1,9 @@
 import Chat from './Chat';
-import TokenInfo from './TokenInfo';
 import './App.css';
-import './TokenInfo.css';
 
 function App() {
   return (
     <div className="App">
-      <TokenInfo />
       <Chat />
     </div>
   );
