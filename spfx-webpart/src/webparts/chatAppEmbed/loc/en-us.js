a2cf37e2507b7f1fe267ba46340f34302e37f7a5
athul-bot89 <athul-bot89@users.noreply.github.com>
2026-05-26 14:22:47 +0530

    revert

--- spfx-webpart/src/webparts/chatAppEmbed/loc/en-us.js
+++
@@ -1,8 +0,0 @@
-define([], function() {
-  return {
-    "PropertyPaneDescription": "Configure the Chat App Embed web part",
-    "BasicGroupName": "App Settings",
-    "IframeUrlFieldLabel": "React App URL",
-    "ApiResourceUriFieldLabel": "Backend API Resource URI"
-  }
-});
